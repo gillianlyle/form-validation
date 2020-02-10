5e8e607a00c73921eaa74070e01fb4a35eecc56e
Gillian Lyle <gillian.lyle@mcdermottgroup.co.uk>
2020-02-10 08:55:15 +0000

    basic form created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
+const App = () => {
+
+  const [value, setValue] = useState({});
+  const handleChange = event => setValue(event.target.value)
+
+  const onSubmit = event => { 
+    event.preventDefault(); // prevent default action
+  } 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <form onSubmit={onSubmit} noValidate>
+          <label>Name: <input type="text" name="name" onChange={handleChange} value={value.name} required /></label>
+          <label>Email: <input type="email" name="email" onChange={handleChange} value={value.email} required /></label>
+          <input type="submit" value="Submit" />
+        </form>
     </div>
   );
 }
